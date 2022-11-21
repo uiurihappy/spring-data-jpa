6da6e4c27e52d0e195d59e67d5c15dd9eb5e6e13
uiurihappy <uiurihappy@sju.ac.kr>
2022-11-21 16:07:11 +0900

    feat: common interface by jpa

---
+++ src/main/java/study/datajpa/repository/TeamJpaRepository.java
@@ -0,0 +1,46 @@
+package study.datajpa.repository;
+
+import org.springframework.stereotype.Repository;
+import study.datajpa.entity.Team;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public class TeamJpaRepository {
+
+	@PersistenceContext
+	private EntityManager em;
+
+	public Team save(Team team) {
+		em.persist(team);
+		return team;
+	}
+
+	public void delete(Team team){
+		em.remove(team);
+	}
+
+	public List<Team> findAll(){
+		return em.createQuery( "select t from Team t", Team.class).getResultList();
+	}
+
+	// Optional로 조회
+	public Optional<Team> findById(Long id) {
+		Team team = em.find(Team.class, id);
+		// member가 null일수도 아닐 수도 있다.
+		return Optional.ofNullable(team);
+	}
+
+	// count
+	public long count(){
+		// 단건인 경우에 대해서는 SingleResult를 사용
+		return em.createQuery(
+				"select count(t)" +
+						"from Team t",
+				Long.class
+		).getSingleResult();
+	}
+}
--- src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -7,6 +7,8 @@ import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 import study.datajpa.entity.Member;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
@@ -18,6 +20,8 @@ class MemberRepositoryTest {
 
 	@Test
 	public void testMember() {
+		System.out.println("memberRepository = " + memberRepository.getClass());
+
 		Member member = new Member("memberA");
 		Member savedMember = memberRepository.save(member);
 
@@ -26,6 +30,43 @@ class MemberRepositoryTest {
 		assertEquals(findMember.getId(), member.getId());
 		assertEquals(findMember.getUsername(), member.getUsername());
 		assertEquals(findMember, member);
+	}
+
+
+	// Spring data jpa도 순수 jpa repository랑 똑같이 복붙했지만 테스트가 성공한다.
+	@Test
+	public void basicCRUD() {
+		Member member1 = new Member("member1");
+		Member member2 = new Member("member2");
+
+		memberRepository.save(member1);
+		memberRepository.save(member2);
+
+		// 단건 조회 검증
+		Member findMember1 = memberRepository.findById(member1.getId()).get();
+		Member findMember2 = memberRepository.findById(member2.getId()).get();
+
+		assertEquals(findMember1, member1);
+		assertEquals(findMember2, member2);
+
+		// 더티 체킹 발생!
+		findMember1.setUsername("member!!!!!!");
+
+		// 리스트 조회 검증
+		List<Member> all = memberRepository.findAll();
+		assertEquals(all.size(), 2);
+
+		// 카운트 검증
+		long count = memberRepository.count();
+		assertEquals(count, 2);
+
+		// 식제 검증
+		memberRepository.delete(member1);
+		memberRepository.delete(member2);
+
+		long deletedCount = memberRepository.count();
+		assertEquals(deletedCount, 0);
+
 	}
 
 }
